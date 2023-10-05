2248e7196659a91a24db6e959796d201953430bf
JonathanKlimp <klimp92@gmail.com>
2023-10-05 14:28:27 +0200

    initial front and backend commit

---
+++ boodschappenVergelijkerBackend/src/main/java/jonathan/jaron/boodschappenVergelijkerBackend/BoodschappenVergelijkerBackendApplication.java
@@ -0,0 +1,13 @@
+package jonathan.jaron.boodschappenVergelijkerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoodschappenVergelijkerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoodschappenVergelijkerBackendApplication.class, args);
+	}
+
+}
