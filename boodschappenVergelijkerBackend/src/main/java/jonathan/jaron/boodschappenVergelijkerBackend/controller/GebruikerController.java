3f9eb6fae0d9072c06844abe2702a0b758d9ba3e
JonathanKlimp <klimp92@gmail.com>
2023-10-17 13:58:12 +0200

    Gebruiker/Gebruiker DTO

--- boodschappenVergelijkerBackend/src/main/java/jonathan/jaron/boodschappenVergelijkerBackend/controller/GebruikerController.java
+++ boodschappenVergelijkerBackend/src/main/java/jonathan/jaron/boodschappenVergelijkerBackend/controller/GebruikerController.java
@@ -1,52 +1,59 @@
 package jonathan.jaron.boodschappenVergelijkerBackend.controller;
 
-//import jonathan.jaron.boodschappenVergelijkerBackend.mapper.ProductMapper;
-//import jonathan.jaron.boodschappenVergelijkerBackend.model.Gebruiker;
-//import jonathan.jaron.boodschappenVergelijkerBackend.model.Product;
-//import jonathan.jaron.boodschappenVergelijkerBackend.model.ProductDto;
-//import jonathan.jaron.boodschappenVergelijkerBackend.repository.GebruikerRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.List;
+import jonathan.jaron.boodschappenVergelijkerBackend.mapper.ProductMapper;
+import jonathan.jaron.boodschappenVergelijkerBackend.model.Gebruiker;
+import jonathan.jaron.boodschappenVergelijkerBackend.model.Product;
+import jonathan.jaron.boodschappenVergelijkerBackend.model.ProductDto;
+import jonathan.jaron.boodschappenVergelijkerBackend.repository.GebruikerRepository;
+import jonathan.jaron.boodschappenVergelijkerBackend.service.GebruikerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
+import java.util.List;
+
+@RestController
 public class GebruikerController {
-//
-//    @Autowired
-//    GebruikerRepository gebruikerRepository;
-//
-//    @Autowired
-//    private ProductMapper productMapper;
-//
-////    @PostMapping("/gebruiker")
-////    Gebruiker save(@RequestBody Gebruiker gebruiker){ return gebruikerRepository.save(gebruiker);}
-//    @GetMapping("/gebruiker")
-//    List<Gebruiker> findAll(){return gebruikerRepository.findAll();}
-//
-//    @PostMapping("/registreer")
-//    public Gebruiker save(@RequestBody Gebruiker gebruiker) {
-//        System.out.println("in registreer user");
-//        System.out.println(gebruiker.getWachtwoord());
-////        gebruiker.setWachtwoord(passwordEncoder.encode(gebruiker.getWachtwoord()));
-//        System.out.println(gebruiker.getWachtwoord());
-//        for (Product product: gebruiker.getProducten() ) {
-//            ProductDto productDto = productMapper.toProductDto(product);
-//        }
-//
-//
-//
-//        return gebruikerRepository.save(gebruiker);
-//    }
-//
-//    @PostMapping("/login")
-//    public ResponseEntity<?> loginUser(@RequestBody Gebruiker gebruiker) {
-//        // Implement user authentication logic, validate credentials
-//        // Return a success or error response, possibly with a JWT token for authentication
-//        return null;
-//    }
+
+    @Autowired
+    GebruikerService gebruikerService;
+
+    @Autowired
+    GebruikerRepository gebruikerRepository;
+
+    @GetMapping("/gebruiker")
+    List<Gebruiker> findAll(){
+        return gebruikerService.findAll();
+    }
+
+    @PostMapping("/registreer")
+    public Gebruiker save(@RequestBody Gebruiker gebruiker) {
+        System.out.println("in registreer user");
+        System.out.println(gebruiker.getWachtwoord());
+        // TODO: implement password encoder
+//        gebruiker.setWachtwoord(passwordEncoder.encode(gebruiker.getWachtwoord()));
+        System.out.println(gebruiker.getWachtwoord());
+
+//        return gebruikerService.save(gebruiker);
+        return gebruikerRepository.save(gebruiker);
+    }
+
+    @PostMapping("/toevoegen")
+    public Gebruiker add(@RequestBody Gebruiker gebruiker) {
+        Gebruiker gebruiker1 = gebruikerRepository.findByGebruikersnaam(gebruiker.getGebruikersnaam());
+        
+        gebruiker1.addProduct(ProductMapper.toListProductDto(gebruiker.getProducten()));
+
+        return gebruiker1;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody Gebruiker gebruiker) {
+        // Implement user authentication logic, validate credentials
+        // Return a success or error response, possibly with a JWT token for authentication
+        return null;
+    }
 }
---
+++ boodschappenVergelijkerBackend/src/main/java/jonathan/jaron/boodschappenVergelijkerBackend/service/GebruikerService.java
@@ -0,0 +1,34 @@
+package jonathan.jaron.boodschappenVergelijkerBackend.service;
+
+//import jonathan.jaron.boodschappenVergelijkerBackend.mapper.GebruikerMapper;
+import jonathan.jaron.boodschappenVergelijkerBackend.model.Gebruiker;
+import jonathan.jaron.boodschappenVergelijkerBackend.model.GebruikerDto;
+import jonathan.jaron.boodschappenVergelijkerBackend.repository.GebruikerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class GebruikerService {
+
+    @Autowired
+    GebruikerRepository gebruikerRepository;
+
+//    public Gebruiker save(Gebruiker gebruiker) {
+//        GebruikerDto gebruikerDto = gebruikerRepository.save(GebruikerMapper.toGebruikerDto(gebruiker));
+//        return GebruikerMapper.toGebruiker(gebruikerDto);
+//    }
+
+    public List<Gebruiker> findAll() {
+        List<Gebruiker> gebruikers = new ArrayList<>();
+
+        List<Gebruiker> gebruikerDtos = gebruikerRepository.findAll();
+//        for (GebruikerDto gebruikerDto: gebruikerDtos) {
+//            gebruikers.add(GebruikerMapper.toGebruiker(gebruikerDto));
+//        }
+
+        return gebruikerRepository.findAll();
+    }
+}
